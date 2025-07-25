dead3a62df61005a2d0f4cd414d15480a1348225
Richard Awayan <awayanrichardjr@gmail.com>
2025-07-25 14:50:40 +0800

    Add initial HTML structure, CSS styles, and JavaScript for portfolio website

---
+++ hardric portfolio/script/function/functions.js
@@ -0,0 +1,6 @@
+function nav_menu() {
+  document.getElementById("nav-sidebar").style.display = "block";
+}
+function nav_sidebar() {
+  document.getElementById("nav-sidebar").style.display = "none";
+}
