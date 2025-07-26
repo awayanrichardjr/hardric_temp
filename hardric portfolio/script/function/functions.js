1413052cadda000136ad9da92bf17ee17c4fd8ec
Richard Awayan <awayanrichardjr@gmail.com>
2025-07-26 18:48:10 +0800

    07-26

--- hardric portfolio/script/function/functions.js
+++
@@ -1,6 +0,0 @@
-function nav_menu() {
-  document.getElementById("nav-sidebar").style.display = "block";
-}
-function nav_sidebar() {
-  document.getElementById("nav-sidebar").style.display = "none";
-}
